3aaa7c735bc0d6d6707be10d07340c7a394950b2
erhardik <you@example.com>
2026-06-26 23:12:01 +0530

    fix: require=False for item_id in formset form to prevent validation failure on extra blank forms

--- apps/requirements/forms.py
+++ apps/requirements/forms.py
@@ -121,7 +121,7 @@ class RequirementCollectionHeaderForm(BootstrapModelForm):
 
 
 class RequirementCollectionItemForm(forms.Form):
-    item_id = forms.IntegerField(widget=forms.HiddenInput())
+    item_id = forms.IntegerField(required=False, widget=forms.HiddenInput())
     quantity_choices = [(str(index), str(index)) for index in range(0, 101)]
     required_qty = forms.TypedChoiceField(
         choices=quantity_choices,
